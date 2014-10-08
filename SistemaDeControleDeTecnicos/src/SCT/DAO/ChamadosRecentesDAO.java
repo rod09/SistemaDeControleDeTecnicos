5fa1baa860001bac3ea2a82a4d0115c938c26639
Rodney <rodney.agro@gmail.com>
2014-10-08 11:46:36 -0300

    Criação de consulta aos chamados mais recentes

--- SistemaDeControleDeTecnicos/src/SCT/DAO/ChamadosRecentesDAO.java
+++ SistemaDeControleDeTecnicos/src/SCT/DAO/ChamadosRecentesDAO.java
@@ -4,14 +4,12 @@
  */
 package SCT.DAO;
 
-import SCT.Classe.Chamado;
 import SCT.Classe.ChamadosRecentes;
 import SCT.Classe.IKey;
 import SCT.Utilidade.EstruturaPesquisa;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
@@ -44,66 +42,7 @@ public class ChamadosRecentesDAO extends PadraoDAO{
         pst.close();
         return chamados;
     }
-
-    @Override
-    public void add(IKey dado) throws SQLException {
-
-        String sql = 
-                "INSERT INTO chamado("
-                + "codigo,"
-                + "tecnico_codigo,"
-                + "maquina_codigo,"
-                + "titulo,"
-                + "descricao,"
-                + "data"
-                + ") VALUES (?, ?, ?, ?, ?, ?)";
-        PreparedStatement pst = connection.prepareStatement(sql);
-
-        //SETA OS VALORES
-        pst.setString(1, dado.getIndex());
-        pst.setString(2, ((Chamado)dado).getCodigoTecnico());
-        pst.setString(3, ((Chamado)dado).getCodigoMaquina());
-        pst.setString(4, ((Chamado)dado).getTitulo());
-        pst.setString(5, ((Chamado)dado).getDescricao());
-        pst.setTimestamp(6, ((Chamado)dado).getData());
-        //EXECUTA
-        pst.executeUpdate();
-        pst.close();
-    }
-
-    @Override
-    public void edit(IKey dado) throws SQLException {
-
-        String sql = "UPDATE chamado SET "
-                + "titulo= ?,"
-                + "descricao= ?,"
-                + "data= ? "
-                + "WHERE codigo= ? AND "
-                + "tecnico_codigo= ? AND "
-                + "maquina_codigo= ? AND ";
-
-        PreparedStatement pst = connection.prepareStatement(sql);
-        pst.setString(1, ((Chamado)dado).getTitulo());
-        pst.setString(2, ((Chamado)dado).getDescricao());
-        pst.setTimestamp(3, ((Chamado)dado).getData());
-        pst.setString(4, dado.getIndex());
-        pst.setString(5, ((Chamado)dado).getCodigoTecnico());
-        pst.setString(6, ((Chamado)dado).getCodigoMaquina());
-
-        pst.executeUpdate();
-        pst.close();
-    }
-
-    @Override
-    public void remove(IKey dado) throws SQLException {
-        String sql = "DELETE FROM chamado WHERE codigo= ?";
-        PreparedStatement pst = connection.prepareStatement(sql);
-        pst.setString(1, dado.getIndex());
-        pst.execute();
-        pst.close();
-    }
-
-   
+ 
     @Override
     public ArrayList load(EstruturaPesquisa pesquisa) throws SQLException {
         ArrayList<ChamadosRecentes> chamados = new ArrayList<>();
@@ -132,24 +71,6 @@ public class ChamadosRecentesDAO extends PadraoDAO{
     }
 
     
-    @Override
-    public IKey consulta(String chave) throws SQLException {
-         Chamado chamado = null;
-        String sql = "SELECT * FROM chamado WHERE codigo = '" + chave + "'";
-
-        Statement stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery(sql);
-        while (rs.next()) {
-            chamado = new Chamado();
-            chamado.setCodigo(rs.getString("codigo"));
-            chamado.setCodigoTecnico(rs.getString("tecnico_codigo"));
-            chamado.setCodigoMaquina(rs.getString("maquina_codigo"));
-            chamado.setTitulo(rs.getString("titulo"));
-            chamado.setDescricao(rs.getString("descricao"));
-            chamado.setData(rs.getTimestamp("data"));
-        }
-        return chamado;
-    }
 
     @Override
     public ArrayList todosToString(EstruturaPesquisa pesquisa) throws SQLException {
@@ -184,5 +105,25 @@ public class ChamadosRecentesDAO extends PadraoDAO{
         rs.close();
         pst.close();
         return chamados;
+    }
+
+    @Override
+    public void edit(IKey dado) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(IKey dado) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IKey consulta(String chave) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(IKey dado) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
