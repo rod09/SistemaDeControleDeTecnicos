c87f1d4d782f0fbd5bdd9727d1adcd91cfd1cd6f
Rodney <rodney.agro@gmail.com>
2014-11-03 11:28:50 -0200

    Agora código gerado automaticamente será único

--- SistemaDeControleDeTecnicos/src/SCT/Tela/TelaTecnico.java
+++ SistemaDeControleDeTecnicos/src/SCT/Tela/TelaTecnico.java
@@ -31,11 +31,7 @@ public class TelaTecnico extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jBCadasatrarFerramenta = new javax.swing.JButton();
-        jBCadastrarMaquina = new javax.swing.JButton();
         jBCadastrarChamado = new javax.swing.JButton();
-        jBCadastrarTecnicos = new javax.swing.JButton();
-        jBCadastrarFerramentasChamado = new javax.swing.JButton();
         jBSair = new javax.swing.JButton();
         jMBPrincipal = new javax.swing.JMenuBar();
         jMAjuda = new javax.swing.JMenu();
@@ -51,43 +47,11 @@ public class TelaTecnico extends javax.swing.JFrame {
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Cadastrar", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Times New Roman", 1, 14), new java.awt.Color(204, 0, 0))); // NOI18N
 
-        jBCadasatrarFerramenta.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icones/Ferramenta.png"))); // NOI18N
-        jBCadasatrarFerramenta.setText(" Ferramentas");
-        jBCadasatrarFerramenta.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBCadasatrarFerramentaActionPerformed(evt);
-            }
-        });
-
-        jBCadastrarMaquina.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icones/Maquina.png"))); // NOI18N
-        jBCadastrarMaquina.setText("Máquinas");
-        jBCadastrarMaquina.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBCadastrarMaquinaActionPerformed(evt);
-            }
-        });
-
         jBCadastrarChamado.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icones/Chamado.png"))); // NOI18N
         jBCadastrarChamado.setText("Chamados");
         jBCadastrarChamado.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBCadastrarChamadoActionPerformed(evt);
-            }
-        });
-
-        jBCadastrarTecnicos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icones/Tecnico.png"))); // NOI18N
-        jBCadastrarTecnicos.setText("Técnicos");
-        jBCadastrarTecnicos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBCadastrarTecnicosActionPerformed(evt);
-            }
-        });
-
-        jBCadastrarFerramentasChamado.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icones/FerramentaChamado.png"))); // NOI18N
-        jBCadastrarFerramentasChamado.setText("Ferramentas Chamado");
-        jBCadastrarFerramentasChamado.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBCadastrarFerramentasChamadoActionPerformed(evt);
             }
         });
 
@@ -105,33 +69,18 @@ public class TelaTecnico extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jBCadastrarTecnicos, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(jBCadasatrarFerramenta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jBCadastrarChamado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addComponent(jBCadastrarChamado, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(38, 38, 38)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jBCadastrarFerramentasChamado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jBCadastrarMaquina, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jBSair, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jBSair, javax.swing.GroupLayout.DEFAULT_SIZE, 273, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jBCadastrarTecnicos, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jBCadastrarFerramentasChamado, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jBCadasatrarFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jBCadastrarMaquina, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 7, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jBCadastrarChamado, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jBSair, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-                .addGap(21, 21, 21))
+                    .addComponent(jBSair, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jMAjuda.setText("Ajuda");
@@ -143,15 +92,13 @@ public class TelaTecnico extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 44, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 3, Short.MAX_VALUE))
+                .addGap(0, 9, Short.MAX_VALUE))
         );
 
         pack();
@@ -161,30 +108,10 @@ public class TelaTecnico extends javax.swing.JFrame {
         finaliza();
     }//GEN-LAST:event_formWindowClosing
 
-    private void jBCadasatrarFerramentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCadasatrarFerramentaActionPerformed
-        // TODO add your handling code here:
-        new TelaGestaoFerramenta(this, true).setVisible(true);
-    }//GEN-LAST:event_jBCadasatrarFerramentaActionPerformed
-
-    private void jBCadastrarMaquinaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCadastrarMaquinaActionPerformed
-        // TODO add your handling code here:
-        new TelaGestaoMaquina(this, true).setVisible(true);
-    }//GEN-LAST:event_jBCadastrarMaquinaActionPerformed
-
     private void jBCadastrarChamadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCadastrarChamadoActionPerformed
         // TODO add your handling code here:
         new TelaGestaoChamado(this, true).setVisible(true);
     }//GEN-LAST:event_jBCadastrarChamadoActionPerformed
-
-    private void jBCadastrarTecnicosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCadastrarTecnicosActionPerformed
-        // TODO add your handling code here:
-        new TelaGestaoTecnico(this, true).setVisible(true);
-    }//GEN-LAST:event_jBCadastrarTecnicosActionPerformed
-
-    private void jBCadastrarFerramentasChamadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBCadastrarFerramentasChamadoActionPerformed
-        // TODO add your handling code here:
-        new TelaGestaoFerramentaChamado(this, true).setVisible(true);
-    }//GEN-LAST:event_jBCadastrarFerramentasChamadoActionPerformed
 
     private void jBSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBSairActionPerformed
         // TODO add your handling code here:
@@ -196,11 +123,7 @@ public class TelaTecnico extends javax.swing.JFrame {
         System.exit(0);
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jBCadasatrarFerramenta;
     private javax.swing.JButton jBCadastrarChamado;
-    private javax.swing.JButton jBCadastrarFerramentasChamado;
-    private javax.swing.JButton jBCadastrarMaquina;
-    private javax.swing.JButton jBCadastrarTecnicos;
     private javax.swing.JButton jBSair;
     private javax.swing.JMenu jMAjuda;
     private javax.swing.JMenuBar jMBPrincipal;
